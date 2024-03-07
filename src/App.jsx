12910dc1a7fea86ecadd304e718fdb4d7cfdbd9f
Benyapa Khiaotai <benyapa_kh@cmu.ac.th>
2024-03-07 11:13:36 +0700

    try mantine

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+// Import styles of packages that you've installed.
+// All packages except `@mantine/hooks` require styles imports
+import '@mantine/core/styles.css';
+
+import { MantineProvider,Button } from '@mantine/core';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   return (
-    <>
+    <MantineProvider>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -16,8 +21,10 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <p>Choo Choo! This is an example of a Vite + React app running on Railway.</p>
+        <Button variant="filled">Button</Button>
       </div>
-    </>
+    </MantineProvider>
+
   )
 }
 
